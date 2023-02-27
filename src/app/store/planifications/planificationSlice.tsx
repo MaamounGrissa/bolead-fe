6506f4ddc2d191f0ab8577b26eed5848489382eb
Maamoun <grissa.maamoun@gmail.com>
2023-02-27 13:06:52 +0100

    Projet Slice update

--- src/app/store/planifications/planificationSlice.tsx
+++ src/app/store/planifications/planificationSlice.tsx
@@ -1,23 +1,16 @@
+import { initialPlanification } from "@app/utils/constant";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 const initialState: PlanificationState = {
-    planification: {
-        id: '',
-        title: '',
-        startDate: '',
-        endDate: '',
-        duration: 20,
-        type: '',
-        ressource: '',
-        projet: '',
-        notes: '',
-        status: '',
-    },
+    planification: initialPlanification,
     planifications: [
-        {id: 'px0', title: 'Immeuble X9', projet: 'IX9', type: 'Visite Technique', status: 'Terminé', notes: 'Lorem text, test lorem text', ressource: 'xx1', startDate: '2023-02-08T14:00', endDate: '2023-02-08T15:30', duration: 20},
-        {id: 'px1', title: 'Immeuble X1', projet: 'IX1', type: 'Visite Technique', status: 'En cours', notes: 'Lorem text, test lorem text', ressource: 'xx1', startDate: '2023-02-12T11:00', endDate: '2023-02-12T12:00', duration: 20},
-        {id: 'px2', title: 'Immeuble X2', projet: 'IX2', type: 'Visite Technique', status: 'En cours', notes: 'Lorem text, test lorem text', ressource: 'xx2', startDate: '2023-02-12T15:00', endDate: '2023-02-12T16:00', duration: 20},
-        {id: 'px3', title: 'Immeuble X3', projet: 'IX3', type: 'Visite Technique', status: 'Terminé', notes: 'Lorem text, test lorem text', ressource: 'xx3', startDate: '2023-02-07T10:00', endDate: '2023-02-07T11:00', duration: 60},
+        {id: 'px0', title: 'Immeuble X9', projet: 'IX9', type: 'Visite Technique', status: 'Terminé', notes: 'Lorem text, test lorem text', ressource: 'xx1', startDate: '2023-02-27T14:00', endDate: '2023-02-27T15:30', duration: 20, origin: '15 Rue du Buisson aux Fraises batiment b3, 91300 Massy, France', destination: '15 Rue du Buisson aux Fraises batiment b3, 91300 Massy, France', distance: '0 km', trajetDuration: 0, trajetDurationText: '0 min'},
+        {id: 'px1', title: 'Immeuble X1', projet: 'IX1', type: 'Visite Technique', status: 'En cours', notes: 'Lorem text, test lorem text', ressource: 'xx1', startDate: '2023-02-28T11:00', endDate: '2023-02-28T13:00', duration: 20, origin: '15 Rue du Buisson aux Fraises batiment b3, 91300 Massy, France', destination: '15 Rue du Buisson aux Fraises batiment b3, 91300 Massy, France', distance: '0 km', trajetDuration: 0, trajetDurationText: '0 min'},
+        {id: 'px2', title: 'Immeuble X2', projet: 'IX2', type: 'Visite Technique', status: 'En cours', notes: 'Lorem text, test lorem text', ressource: 'xx2', startDate: '2023-03-01T14:00', endDate: '2023-03-01T16:00', duration: 20, origin: '15 Rue du Buisson aux Fraises batiment b3, 91300 Massy, France', destination: '15 Rue du Buisson aux Fraises batiment b3, 91300 Massy, France', distance: '0 km', trajetDuration: 0, trajetDurationText: '0 min'},
+        {id: 'px3', title: 'Immeuble X3', projet: 'IX3', type: 'Visite Technique', status: 'Terminé', notes: 'Lorem text, test lorem text', ressource: 'xx3', startDate: '2023-03-01T10:00', endDate: '2023-03-01T12:00', duration: 60, origin: '15 Rue du Buisson aux Fraises batiment b3, 91300 Massy, France', destination: '15 Rue du Buisson aux Fraises batiment b3, 91300 Massy, France', distance: '0 km', trajetDuration: 0, trajetDurationText: '0 min'},
+        {id: 'px4', title: 'Immeuble X4', projet: 'IX4', type: 'Visite Technique', status: 'Terminé', notes: 'Lorem text, test lorem text', ressource: 'xx4', startDate: '2023-03-01T09:00', endDate: '2023-03-01T11:00', duration: 60, origin: '15 Rue du Buisson aux Fraises batiment b3, 91300 Massy, France', destination: '15 Rue du Buisson aux Fraises batiment b3, 91300 Massy, France', distance: '0 km', trajetDuration: 0, trajetDurationText: '0 min'},
+        {id: 'px5', title: 'Immeuble X5', projet: 'IX5', type: 'Visite Technique', status: 'Terminé', notes: 'Lorem text, test lorem text', ressource: 'xx5', startDate: '2023-02-01T10:00', endDate: '2023-03-01T13:00', duration: 60, origin: '15 Rue du Buisson aux Fraises batiment b3, 91300 Massy, France', destination: '15 Rue du Buisson aux Fraises batiment b3, 91300 Massy, France', distance: '0 km', trajetDuration: 0, trajetDurationText: '0 min'},
+        {id: 'px6', title: 'Immeuble X6', projet: 'IX6', type: 'Visite Technique', status: 'Terminé', notes: 'Lorem text, test lorem text', ressource: 'xx6', startDate: '2023-02-02T10:00', endDate: '2023-03-02T12:00', duration: 60, origin: '15 Rue du Buisson aux Fraises batiment b3, 91300 Massy, France', destination: '15 Rue du Buisson aux Fraises batiment b3, 91300 Massy, France', distance: '0 km', trajetDuration: 0, trajetDurationText: '0 min'},
     ],
 };
 
